cafe1987100e85b4d8cf4f7dcf90bb1ea6655f90
Stephan Lukasczyk <stephan@lukasczyk.me>
2018-11-17 12:24:27 +0100

    Fill init with module information

---
+++ pytesting_utils/__init__.py
@@ -0,0 +1,42 @@
+"""
+PyTesting utils is a collection of utilities for the PyTesting project.
+
+This file is part of PyTesting utils.
+
+PyTesting utils is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PyTesting utils is distributed in the hope that it will be useful
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PyTesting utils.  If not, see <https://www.gnu.org/licenses/>.
+"""
+__all__ = [
+    "cd",
+    "tempdir",
+    "virtualenv",
+    "Preconditions",
+    "IllegalArgumentException",
+    "IllegalStateException",
+    "NoneValueException",
+    "VirtualEnvironment",
+]
+
+from .context_managers import cd, tempdir, virtualenv
+from .preconditions import (
+    Preconditions,
+    IllegalArgumentException,
+    IllegalStateException,
+    NoneValueException,
+)
+from .virtual_environment import VirtualEnvironment
+
+
+__version__ = "0.1"
+__name__ = "pytesting_utils"
+__author__ = "Stephan Lukasczyk"
