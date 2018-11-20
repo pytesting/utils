7d08849ac2d0893809fe8c62ee587f2b2234925d
Stephan Lukasczyk <stephan@lukasczyk.me>
2018-11-20 12:51:34 +0100

    Bump version

--- pytesting_utils/__init__.py
+++ pytesting_utils/__init__.py
@@ -37,6 +37,6 @@ from .preconditions import (
 from .virtual_environment import VirtualEnvironment
 
 
-__version__ = "0.2.dev0"
+__version__ = "0.2.dev1"
 __name__ = "pytesting_utils"
 __author__ = "Stephan Lukasczyk"
